1e4f9fc1b49daca199b2fef1c15b0113fcaa05ff
Mizogg <88630056+Mizogg@users.noreply.github.com>
2023-12-19 16:09:35 +0000

    Add files via upload

---
+++ Solo_Poetry/libs/set_settings.py
@@ -0,0 +1,23 @@
+"""
+
+@author: Team Mizogg
+"""
+from PyQt6.QtWidgets import QMessageBox
+
+CONFIG_FILE = "config/config.json"
+def get_settings():
+    settings_dict = {}
+    try:
+        with open(CONFIG_FILE, "r") as settings_file:
+            for line in settings_file:
+                line = line.strip()
+                if "=" in line:
+                    key, value = line.split("=", 1)
+                    settings_dict[key] = value
+    except FileNotFoundError:
+        setting_message = "Settings file not found."
+        QMessageBox.information(None, "File not found", setting_message)
+    except Exception as e:
+        error_message = f"An error occurred while reading settings: {e}"
+        QMessageBox.critical(None, "Error", error_message)
+    return settings_dict
