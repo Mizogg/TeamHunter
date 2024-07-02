9904309ac0989ad6317d01393d8263f0b871df7c
Mizogg <88630056+Mizogg@users.noreply.github.com>
2024-07-02 20:18:06 +0100

    Delete game directory

--- game/aboutWindow.py
+++
@@ -1,7 +0,0 @@
-from PyQt6.QtWidgets import QMessageBox
-
-def TetrisAboutWindow():
-    msgBox = QMessageBox(QMessageBox.Icon.Information, "Unavailable", "This feature is currently unavailable", QMessageBox.StandardButton.Ok)
-
-    return msgBox
-
