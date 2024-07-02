da24def026c373069494e3aa2c5f7a880faac2da
Mizogg <88630056+Mizogg@users.noreply.github.com>
2024-07-02 20:33:49 +0100

    1.4

---
+++ TeamHunter_Version1.4/pip install.bat
@@ -0,0 +1,13 @@
+python -m pip install --upgrade pip
+pip install PyQt6
+pip install bit
+pip install requests
+pip install base58
+pip install ecdsa
+pip install hdwallet
+pip install mnemonic
+pip install numpy
+pip install keyboard
+pip install pygame
+pip install trotter
+pause
