bebc1a58098a905d9ce30b08c1640800ebf34986
Mizogg <88630056+Mizogg@users.noreply.github.com>
2024-07-02 20:25:28 +0100

    new version

---
+++ TeamHunter_Version1.3/pip install.bat
@@ -0,0 +1,16 @@
+python -m pip install --upgrade pip
+pip install PyQt6
+pip install bit
+pip install QDarkStyle
+pip install requests
+pip install base58
+pip install ecdsa
+pip install simplebloomfilter
+pip install bitarray==1.9.2
+pip install hdwallet
+pip install mnemonic
+pip install numpy
+pip install keyboard
+pip install pygame
+pip install trotter
+pause
